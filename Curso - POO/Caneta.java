cb0b528c7f290441659666a484a0edd0c567291b
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-13 16:30:48 -0300

    classe

---
+++ Curso - POO/Caneta.java
@@ -0,0 +1,37 @@
+package aula02;
+
+public class Caneta {
+	
+	String modelo;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+	void status () {
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Uma caneta " + this.cor);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Carga: " + this.carga);
+		System.out.println(" esta tampada? " + this.tampada);
+	}
+	
+	void rabiscar () {
+		if (this.tampada == true) {
+			System.out.println("ERRO! Nao posso rabiscar");
+		
+		} else {
+			System.out.println("Estou rabiscando");
+		}
+
+	}
+	
+	void tampar () {
+		this.tampada = true; // this é uma referencia ao propio objeto	q é chamado,quando se quer mudar um atributo dentro da propria classe coloque a palavra this na frente da linha que vai mexer nesse atributo.
+		
+	}
+
+	void destampar () {
+		this.tampada = false;
+		
+	}
+}
