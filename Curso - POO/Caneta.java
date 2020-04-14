04aca7833816f7a892ebda5c87c476f9c2c32c52
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-14 19:15:04 -0300

    aula 04 ->classe,métodos getter,setter e contrutor

--- Curso - POO/Caneta.java
+++ Curso - POO/Caneta.java
@@ -1,39 +1,51 @@
-package aula03;
+package aula04;
 
 public class Caneta {
-	
-	public String modelo;
-	public String cor;
-	private float ponta;
-	protected int carga;
+
+	private String modelo;
+	private float ponta; 
+	private String cor;
 	private boolean tampada;
-	public void status () {
-		System.out.println("Modelo: " + this.modelo);
-		System.out.println("Uma caneta " + this.cor);
-		System.out.println("Ponta: " + this.ponta);
-		System.out.println("Carga: " + this.carga);
-		System.out.println(" esta tampada? " + this.tampada);
-	}
 	
-	public void rabiscar () {
-		if (this.tampada == true) {
-			System.out.println("ERRO! Nao posso rabiscar");
+	
+	public Caneta(String m, String c, float p) { // Este é o método Construtor 
+		this.modelo = m;
+		this.cor = c;
+		this.setPonta(p);
+		this.tampar ();
 		
-		} else {
-			System.out.println("Estou rabiscando");
-		}
-
 	}
 	
+	public String getModelo () {
+		return this.modelo;
+	}
+	public void setModelo (String m) {
+		this.modelo = m;
+	}
+	public float getPonta () {
+		return this.ponta;
+	}
+	public void setPonta (float p) {
+		this.ponta = p;
+	}
 	public void tampar () {
-		this.tampada = true; // this é uma referencia ao propio objeto	q é chamado,quando se quer mudar um atributo dentro da propria classe coloque a palavra this na frente da linha que vai mexer nesse atributo.
-		
+		this.tampada = true;
 	}
-
-	 public void destampar () {
+	
+	public void destampar () {
 		this.tampada = false;
-		
 	}
-}	
+	
+	
+	public void status () {
+		System.out.println("SOBRE A CANETA:");
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Tampada: " + this.tampada);
+	}
+	
+	
+}
 
 
