99cfaf94316042882582d4d5924a544086d65446
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-23 19:21:23 -0300

    aula 09 -> Interface

---
+++ Curso - POO/Publicação.java
@@ -0,0 +1,10 @@
+package ProjetoPessoa08;
+
+public interface Publicação {
+	public void abrir();
+	public void fechar();
+	public void folhear(int p);
+	public void avancarPag();
+	public void voltarPag();
+	
+}
