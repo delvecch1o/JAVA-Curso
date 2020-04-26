9a7c36af01b7e4272e266033f9a1c6180897cef5
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-25 23:28:41 -0300

    aula 11 -> classe

--- Curso - POO/Aluno.java
+++ Curso - POO/Aluno.java
@@ -1,29 +1,30 @@
-package projetopessoas;
+package aula011;
 
 public class Aluno extends Pessoa {
+
 	private int matricula;
 	private String curso;
 	
-	public void cancelarMatricula() {
-		System.out.println("Matrícula será cancelada");
+	public void pagarMensalidade() { // public void final pagarMensalidade() se eu colocar o final , ele nao pode ser mais sobreposto, pq o metodo final nao extende a mais nada
+		System.out.println("Pagando mensalidade de aluno " + this.getNome());
 	}
-
+	
 	public int getMatricula() {
 		return matricula;
 	}
-
 	public void setMatricula(int matricula) {
 		this.matricula = matricula;
 	}
-
 	public String getCurso() {
 		return curso;
 	}
-
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
-	
-	
-	
+	/*
+	 aluno tem tudo o que pessoa tem (nome, idade, sexo, fazer aniversario) + suas especialização
+	 então
+	 aluno é uma especialização de pessoa e
+	 pessoa e uma gereneralização de aluno
+	 */
 }
