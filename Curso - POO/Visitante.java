be758595b083b5c2bfcbc55485aa2c126a45fde9
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-25 23:30:50 -0300

    aula 11 -> classe

---
+++ Curso - POO/Visitante.java
@@ -0,0 +1,15 @@
+package aula011;
+
+public class Visitante extends Pessoa {
+
+}
+ /*
+ 	HERANÇA POBRE, HERANÇA DE IMPLEMENTAÇÃO, porque vai herdar tudo o que pessoa tem,
+ 	 que no caso é a sua superClasse, 
+ 	 que tem nome, idade, sexo, fazer aniversario,
+ 	  mas não irá implementar nada novo.
+ 	  
+ 	  PESSOA = SuperClasse
+ 	  VISITANTE = SubClasse
+
+*/
