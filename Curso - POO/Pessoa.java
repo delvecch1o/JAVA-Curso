81bf03fae0835d06ab153c67f9884ab5bf6f76ac
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-25 23:29:39 -0300

    aula 11 -> classe

--- Curso - POO/Pessoa.java
+++ Curso - POO/Pessoa.java
@@ -1,6 +1,6 @@
-package projetopessoas;
+package aula011;
 
-public class Pessoa {
+public abstract class Pessoa {
 	private String nome;
 	private int idade;
 	private String sexo;
@@ -8,37 +8,34 @@ public class Pessoa {
 	public void fazerAniversario() {
 		this.idade++;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public int getIdade() {
 		return idade;
 	}
-
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-
 	public String getSexo() {
 		return sexo;
 	}
-
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
 
+
 	@Override
+	
 	public String toString() {
-		return "Pessoa [nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + "]";
+		System.out.println("--------------------------");
+		return "Pessoa {nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + '}';
+		
 	}
-	
-	
 	
 	
 	
